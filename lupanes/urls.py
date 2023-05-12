02a293672a17c44aaa6cac4fb369e6d922f85925
Santiago Lamora <santiago@ribaguifi.com>
2023-05-12 13:48:39 +0200

    Allow deleting DeliveryNotes

--- lupanes/urls.py
+++ lupanes/urls.py
@@ -7,5 +7,6 @@ app_name = "lupanes"
 urlpatterns = [
     path('auth/login/', views.CustomerLoginView.as_view(), name='customer-login'),
     path('albaran/new/', views.DeliveryNoteCreateView.as_view(), name='deliverynote-new'),
-    path('albaran/<int:pk>/edit/', views.DeliveryNoteUpdateView.as_view(), name='deliverynote-edit')
+    path('albaran/<int:pk>/edit/', views.DeliveryNoteUpdateView.as_view(), name='deliverynote-edit'),
+    path('albaran/<int:pk>/delete/', views.DeliveryNoteDeleteView.as_view(), name='deliverynote-delete'),
 ]
--- lupanes/views.py
+++ lupanes/views.py
@@ -4,7 +4,7 @@ from django.db.models import QuerySet
 from django.utils import timezone
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView, FormView, UpdateView
+from django.views.generic.edit import CreateView, DeleteView, FormView, UpdateView
 
 from lupanes.forms import CustomerAuthForm, DeliveryNoteCreateForm
 from lupanes.models import DeliveryNote
@@ -43,6 +43,15 @@ class DeliveryNoteCreateView(CustomerAuthMixin, CreateView):
 class DeliveryNoteUpdateView(CustomerAuthMixin, UpdateView):
     model = DeliveryNote
     fields = ["product", "quantity"]
+    success_url = reverse_lazy("lupanes:deliverynote-new")
+
+    def get_queryset(self) -> QuerySet[Any]:
+        today = timezone.now().date()
+        return DeliveryNote.objects.filter(customer=self.customer, date__date=today)
+
+
+class DeliveryNoteDeleteView(CustomerAuthMixin, DeleteView):
+    model = DeliveryNote
     success_url = reverse_lazy("lupanes:deliverynote-new")
 
     def get_queryset(self) -> QuerySet[Any]:
