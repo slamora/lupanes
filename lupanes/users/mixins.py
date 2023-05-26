5186e08fbcbb8c200ab039fcb57541981c2e96ea
Santiago Lamora <santiago@ribaguifi.com>
2023-05-26 12:33:08 +0200

    Refactor menu & product_list in base of user groups

--- lupanes/users/mixins.py
+++ lupanes/users/mixins.py
@@ -6,13 +6,23 @@ CUSTOMERS_GROUP = "neveras"
 MANAGERS_GROUP = "tienda"
 
 
-class CustomerAuthMixin(UserPassesTestMixin):
+# TODO(@slamora): move this methods to custom user model!!!!
+class LupierraBaseAuth(UserPassesTestMixin):
     def test_func(self):
         user = self.request.user
-        return user.is_authenticated and user.groups.filter(name=CUSTOMERS_GROUP).exists()
+        user.is_customer = user.groups.filter(name=CUSTOMERS_GROUP).exists()
+        user.is_manager = user.groups.filter(name=MANAGERS_GROUP).exists()
 
 
-class ManagerAuthMixin(UserPassesTestMixin):
+class CustomerAuthMixin(LupierraBaseAuth):
     def test_func(self):
+        super().test_func()
         user = self.request.user
-        return user.is_authenticated and user.groups.filter(name=MANAGERS_GROUP).exists()
+        return user.is_authenticated and user.is_customer
+
+
+class ManagerAuthMixin(LupierraBaseAuth):
+    def test_func(self):
+        super().test_func()
+        user = self.request.user
+        return user.is_authenticated and user.is_manager
