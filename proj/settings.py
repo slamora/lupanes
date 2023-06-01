610669a1238876633eb25782fedaaa3b8fc780cc
Santiago Lamora <santiago@ribaguifi.com>
2023-06-01 14:05:08 +0200

    Add settings to configure sentry.io (optional)

--- proj/settings.py
+++ proj/settings.py
@@ -26,6 +26,27 @@ env = environ.Env(
 )
 environ.Env.read_env(Path(BASE_DIR, '.env'))
 
+# sentry.io app monitoring platform (optional)
+if env('SENTRY_DSN', default=None):
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=env('SENTRY_DSN'),
+        integrations=[
+            DjangoIntegration(),
+        ],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=0.2,
+
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True
+    )
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
