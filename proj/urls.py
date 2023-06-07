e350df7bcf65964424be4d3d8a9cb4ad1c3e7ba1
Santiago Lamora <santiago@ribaguifi.com>
2023-06-08 00:33:29 +0200

    Enable contrib.admin

--- proj/urls.py
+++ proj/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('turberes/', admin.site.urls),
     path('', include('lupanes.urls')),
     path('', include('lupanes.users.urls')),
     path('', RedirectView.as_view(pattern_name='users:login', permanent=True), name='root_index'),
