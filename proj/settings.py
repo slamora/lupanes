f956faa9cb76cf25d7ea4e919b5c5a6c36a6f808
Santiago Lamora <santiago@ribaguifi.com>
2023-06-15 14:03:06 +0200

    Bugfix: always format date following user locale
    WARNING: on Django 3.2 USE_L10N is False by default but since 4.0 default is True

--- proj/settings.py
+++ proj/settings.py
@@ -148,6 +148,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+# NOTE WARNING: on Django 3.2 USE_L10N is False by default but since 4.0 default is True
+USE_L10N = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
