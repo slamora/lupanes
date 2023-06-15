1ea9ae79ee6b3c95bc5916d083c1e265b985f73a
Santiago Lamora <santiago@ribaguifi.com>
2023-06-15 13:15:15 +0200

    Force language to be always `es` (Spanish)

--- proj/settings.py
+++ proj/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 
 import environ
 from django.utils.crypto import get_random_string
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,6 +142,10 @@ AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'es-es'
+
+LANGUAGES = [
+    ("es", _("Spanish")),
+]
 
 TIME_ZONE = 'Europe/Madrid'
 
