661d682662e058b1c5c5bc4ed7a0747c32f731bb
Santiago Lamora <santiago@ribaguifi.com>
2023-11-09 12:57:26 +0100

    Fix icon paths on pwa manifest

--- proj/settings.py
+++ proj/settings.py
@@ -254,12 +254,12 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        "src": "/android-chrome-192x192.png",
+        "src": "/static/android-chrome-192x192.png",
         "sizes": "192x192",
         "type": "image/png"
     },
     {
-        "src": "/android-chrome-512x512.png",
+        "src": "/static/android-chrome-512x512.png",
         "sizes": "512x512",
         "type": "image/png"
     }
